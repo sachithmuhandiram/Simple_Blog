6175b0b230d63b1e492471596dd1192b6b39ee37
sachithnalaka <sachithnalaka@gmail.com>
2016-12-22 20:34:14 +0530

    Http404 error message was created

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 #from django.template import loader # replaced by render
 from django.shortcuts import render
@@ -8,7 +9,7 @@ from .models import Authors #importing models
 def index(request):
 	all_authors = Authors.objects.all()		#getting all author details
 	#author_template = loader.get_template('authors/author_index.html') 	#loading the view
-	#creating a dictionary
+	#creating a dictionary 
 	author_data ={
 		'all_authors' : all_authors,
 	}
@@ -16,7 +17,13 @@ def index(request):
 	#render function takes (request,'link to our view', data for the view)
 
 def authors_details(request,authors_id):
-	return HttpResponse("<h1>Author ID is : "+str(authors_id)+"</h2>")
+	try:
+		author_details=Authors.objects.get(id=authors_id)
+	except Authors.DoesNotExist:
+		raise Http404("That author is not availble here")
+	return render(request,'authors/author_detail.html',{'author_details':author_details})
+	
+	
 
 
 
