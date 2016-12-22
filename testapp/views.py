84ea9775ff5e99278c33afc90d599fe197f0dbd7
sachithnalaka <sachithnalaka@gmail.com>
2016-12-22 18:01:44 +0530

    render function is used for returning html views

--- testapp/views.py
+++ testapp/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
-from django.template import loader #
+#from django.template import loader # replaced by render
+from django.shortcuts import render
 from .models import Authors #importing models
 
 
 # Create your views here.
 def index(request):
 	all_authors = Authors.objects.all()		#getting all author details
-	author_template = loader.get_template('authors/author_index.html') 	#loading the view
+	#author_template = loader.get_template('authors/author_index.html') 	#loading the view
 	#creating a dictionary
 	author_data ={
 		'all_authors' : all_authors,
 	}
-	return HttpResponse(author_template.render(author_data,request)) #returning the html view
+	return render(request,'authors/author_index.html',author_data) #HttpResponse(author_template.render(author_data,request)) #returning the html view
+	#render function takes (request,'link to our view', data for the view)
 
 def authors_details(request,authors_id):
 	return HttpResponse("<h1>Author ID is : "+str(authors_id)+"</h2>")
